a024684557a1b1754c113abc16314ff5ef13935e
Arya Chakraborty <arya021chakraborty@gmail.com>
2026-03-06 20:42:55 +0530

    created post route

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,17 +1,31 @@
 const ImageKit= require("@imagekit/nodejs");
 const  { toFile } = require("@imagekit/nodejs");
+const jwt = require("jsonwebtoken");
+const postModel = require("../models/post.model");
 const client = new ImageKit({
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY, 
 });
 
 async function postCreate(req,res){
-    console.log(req.body);
-    console.log(req.file);
-   const file = await client.files.upload({
+    const token = req.cookies.jwt;
+    let isValidUser = null;
+    try{
+     isValidUser = jwt.verify(token,process.env.JWT_SECRET);
+    }
+    catch (error){
+        return res.send("Unotherized Access");
+    }
+  const result =  await client.files.upload({
   file: await toFile(Buffer.from(req.file.buffer), 'file'),
   fileName: 'image',
 });
-res.send(file);
+    const post = await postModel.create({
+        caption: req.body.caption,
+        imageUrl: result.url,
+        user:isValidUser.id,
+    });
+    console.log(post);
+    res.send("post created")
 }
 
 module.exports ={
