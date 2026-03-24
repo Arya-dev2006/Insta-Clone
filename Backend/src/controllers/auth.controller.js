92ba973d05f39c5fc46e744bed818ef4170d6653
Arya Chakraborty <arya021chakraborty@gmail.com>
2026-03-24 09:54:29 +0530

    created getme route

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -60,7 +60,19 @@ async function loginController(req,res){
 
     res.send("login successful");
 }
+async function getMeController(req,res){
+    const userId = req.user.id;
+    const user = await userModel.findById(userId);
+    res.status(200).json({
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        bio: user.bio,
+        profileImage: user.profileImage,
+    });
+}
 module.exports = {
     registerController,
     loginController,
+    getMeController,
 };
--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const identifyUser = require("../middleware/auth.middleware");
 const authRouter = express.Router();
 const authController = require("../controllers/auth.controller")
 authRouter.post('/register',authController.registerController);
-authRouter.post('/login',authController.loginController)
+authRouter.post('/login',authController.loginController);
+authRouter.get('/get-me',identifyUser,authController.getMeController);
 module.exports = authRouter;
